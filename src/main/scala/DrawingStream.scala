83508868234ed1997547867db2ed5558331acb0c
Davi Suga <daviciencia1@gmail.com>
2022-12-01 19:31:01 -0300

    feat: created stream with draw events

---
+++ src/main/scala/DrawingStream.scala
@@ -0,0 +1,15 @@
+package multiplayer_canvas.drawing_stream
+import zio._
+import zio.Console._
+import zio.stream._
+import zio.Duration._
+import multiplayer_canvas.types._
+
+private val queue = Queue.bounded[Draw](100)
+
+def getDrawingStream() = queue.map(queue => ZStream.fromQueue(queue))
+
+def subscribe(handler: (Draw => ZIO[Any, Nothing, Any])) =
+  getDrawingStream().flatMap(_.foreach(handler))
+
+def addDraw(draw: Draw) = queue.flatMap(q => q.offer(draw))
