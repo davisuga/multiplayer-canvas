716b06234f3eff2bade521e657e35f55fe09b6f3
Davi Suga <daviciencia1@gmail.com>
2022-12-01 19:31:46 -0300

    feat: ZIO http + Caliban

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,31 @@
-@main def hello: Unit = 
-  println("Hello world!")
-  println(msg)
+import zio._
+import multiplayer_canvas.graphql._
 
-def msg = "I was compiled by Scala 3. :)"
+import caliban.ZHttpAdapter
+import io.netty.handler.codec.http.{HttpHeaderNames, HttpHeaderValues}
+import zio._
+import zio.stream._
+import zhttp.http._
+import zhttp.service.Server
+
+object MultiplayerCanvas extends ZIOAppDefault {
+  private val graphiql = Http.fromStream(ZStream.fromResource("graphiql.html"))
+
+  override val run =
+    println("RUNNING THIS SHIT")
+    (for {
+      interpreter <- getGraphQLInterpreter()
+      _ <- Server
+        .start(
+          8088,
+          Http.collectHttp[Request] {
+            case _ -> !! / "api" / "graphql" =>
+              ZHttpAdapter.makeHttpService(interpreter)
+            case _ -> !! / "ws" / "graphql" =>
+              ZHttpAdapter.makeWebSocketService(interpreter)
+            case _ -> !! / "graphiql" => graphiql
+          }
+        )
+        .forever
+    } yield ()).exitCode
+}
