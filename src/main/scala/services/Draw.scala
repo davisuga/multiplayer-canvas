f685766b3f7e9c6da89018c64d685132d3e3bc48
Davi Suga <daviciencia1@gmail.com>
2022-12-02 16:08:00 -0300

    feat: effectively adding the item to the queue

--- src/main/scala/services/Draw.scala
+++ src/main/scala/services/Draw.scala
@@ -2,12 +2,15 @@ package multiplayer_canvas.draw.service
 
 import multiplayer_canvas.drawing_stream._
 import multiplayer_canvas.types._
+import zio._
+import util.chaining.scalaUtilChainingOps
 
-def draw(value: Draw) =
-  queueDraw(value)
-  multiplayer_canvas.InMemory.writePixel(
-    value.x,
-    value.y,
-    value.pixel,
-    value.canvasId
-  )
+def draw(queue: Queue[Draw])(value: Draw) =
+  multiplayer_canvas.InMemory
+    .writePixel(
+      value.x,
+      value.y,
+      value.pixel,
+      value.canvasId
+    )
+    .tap(_ => queueDraw(value)(queue))
