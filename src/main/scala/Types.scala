69cc275d9bbbaaf7fbf9a7a8c8f8e91e8687d5c9
Davi Suga <daviciencia1@gmail.com>
2022-12-01 21:18:09 -0300

    fix: fixed size and position types

--- src/main/scala/Types.scala
+++ src/main/scala/Types.scala
@@ -3,11 +3,11 @@ import zio.stream.ZStream
 import zio._
 
 type ID = String
+case class Size(rows: Int, columns: Int)
 
 case class Color(r: Int, g: Int, b: Int)
-case class Pixel(x: Int, y: Int, color: Color)
+case class Pixel(color: Color)
 case class Canvas(id: ID, value: List[List[Pixel]])
 
-trait Action
-case class Draw(canvasId: ID, value: Pixel, timestamp: Int) extends Action
-case class GetCanvas(canvasId: ID) extends Action
+case class Draw(canvasId: ID, pixel: Pixel, timestamp: Int, x: Int, y: Int)
+case class GetCanvas(canvasId: ID)
