80540215a7e49a43ec80a278c32649da08a9da56
Davi Suga <daviciencia1@gmail.com>
2022-12-02 10:04:32 -0300

    feat: simple draw test

---
+++ src/test/scala/Canvas.scala
@@ -0,0 +1,41 @@
+import zio._
+import zio.test.{test, _}
+import zio.test.Assertion._
+import multiplayer_canvas.types._
+import multiplayer_canvas._
+
+object CanvasSpec extends ZIOSpecDefault {
+  def spec =
+    suite("CanvasSpec")(
+      test("Draw a pixel") {
+        val empty = Canvas("0", createEmptyCanvas(3, 4))
+        val modified =
+          addPixelToCanvas(empty, Pixel(Color(255, 255, 255)), 0, 0)
+        assertTrue(
+          modified == Canvas(
+            "0",
+            List(
+              List(
+                Pixel(Color(255, 255, 255)),
+                Pixel(Color(0, 0, 0)),
+                Pixel(Color(0, 0, 0)),
+                Pixel(Color(0, 0, 0))
+              ),
+              List(
+                Pixel(Color(0, 0, 0)),
+                Pixel(Color(0, 0, 0)),
+                Pixel(Color(0, 0, 0)),
+                Pixel(Color(0, 0, 0))
+              ),
+              List(
+                Pixel(Color(0, 0, 0)),
+                Pixel(Color(0, 0, 0)),
+                Pixel(Color(0, 0, 0)),
+                Pixel(Color(0, 0, 0))
+              )
+            )
+          )
+        )
+      }
+    )
+}
