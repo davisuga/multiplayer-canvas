1ead2abeb1b2fd97b562b2b01a5b0157d8c7df30
Davi Suga <daviciencia1@gmail.com>
2022-12-02 16:13:37 -0300

    feat: types refac

--- src/main/scala/entities/Canvas.scala
+++ src/main/scala/entities/Canvas.scala
@@ -2,6 +2,10 @@ package multiplayer_canvas
 
 import multiplayer_canvas.types._
 
+def createEmptyCanvas(rows: Int, cols: Int) = List.fill(rows) {
+  List.fill(cols) { Pixel(Color(0, 0, 0)) }
+}
+
 def addPixelToCanvas(canvas: Canvas, pixel: Pixel, x: Int, y: Int) =
   Canvas(
     canvas.id,
