621d1a80948b739987f7f24504c6f2da6c22dfeb
Davi Suga <daviciencia1@gmail.com>
2022-12-03 18:34:57 -0300

    feat: event type

--- src/main/scala/entities/Types.scala
+++ src/main/scala/entities/Types.scala
@@ -9,5 +9,10 @@ case class Color(r: Int, g: Int, b: Int)
 case class Pixel(color: Color)
 case class Canvas(id: ID, value: List[List[Pixel]])
 
+sealed trait Event
 case class Draw(canvasId: ID, pixel: Pixel, timestamp: Int, x: Int, y: Int)
+    extends Event
+case class Enter() extends Event
+case class Unknown() extends Event
+
 case class GetCanvas(canvasId: ID)
