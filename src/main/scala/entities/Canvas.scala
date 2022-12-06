1554cbda8800c8487b6d9ff89346043e3fcd6a2d
Davi Suga <daviciencia1@gmail.com>
2022-12-06 18:29:07 -0300

    fix: fixed referencing

--- src/main/scala/entities/Canvas.scala
+++ src/main/scala/entities/Canvas.scala
@@ -1,23 +1,25 @@
 package entities
-import entities.types.{Pixel, Color}
+import entities.types.{Color, Pixel}
 object Canvas {
 
-  def createEmptyCanvas(rows: Int, cols: Int) = List.fill(rows) {
-    List.fill(cols) { Pixel(Color(0, 0, 0)) }
-  }
+  def createEmptyCanvas(rows: Int, cols: Int) =
+    val emptyPixel = Pixel(Color(0, 0, 0))
+    val emptyRow = List.fill(cols) { emptyPixel }
+
+    List.fill(rows) {
+      emptyRow
+    }
 
   def addPixelToCanvas(
-      canvas: entities.types.Canvas,
-      pixel: Pixel,
-      x: Int,
-      y: Int
+    canvas: entities.types.Canvas,
+    pixel: Pixel,
+    x: Int,
+    y: Int
   ) =
     entities.types.Canvas(
       canvas.id,
       canvas.value.zipWithIndex.map((row, i) =>
-        row.zipWithIndex.map((elem, j) =>
-          if j == x && i == y then pixel else elem
-        )
+        row.zipWithIndex.map((elem, j) => if j == x && i == y then pixel else elem)
       )
     )
 
