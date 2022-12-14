d65977eaaab373bee6b7235af8596f6a6455b6dd
Davi Suga <daviciencia1@gmail.com>
2022-12-14 19:24:08 -0300

    feat: canvas service

---
+++ src/main/scala/services/Canvas.scala
@@ -0,0 +1,23 @@
+package services
+import entities.{Canvas, Event}
+import cats.effect.IO
+
+object Canvas {
+  def read(canvasId: String) =
+    models.redis.client.use { client =>
+      given models.redis.RedisEnv = client
+      models.redis.Canvas.read(canvasId)
+    }
+  def create(rows: Int, cols: Int) =
+    val id = java.util.UUID.randomUUID().toString()
+    val emptyCanvas = entities.Canvas(id, entities.Canvas.empty(cols, rows))
+    models.redis.client.use { client =>
+      given models.redis.RedisEnv = client
+      models.redis.Canvas.create(emptyCanvas) >> services.addTopic(id) >> IO.pure(id)
+    }
+  def draw(canvas: Event.Draw) =
+    models.redis.client.use { client =>
+      given models.redis.RedisEnv = client
+      models.redis.Canvas.draw(canvas)
+    }
+}
