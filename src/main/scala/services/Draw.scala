c3fc0097985abf56d3527b6b0ebe71bdaa6f2853
Davi Suga <daviciencia1@gmail.com>
2022-12-14 19:24:15 -0300

    feat: draw service

--- src/main/scala/services/Draw.scala
+++ src/main/scala/services/Draw.scala
@@ -1,15 +1,27 @@
-package draw.service
+package services
 
 import entities._
-import zio._
-import util.chaining.scalaUtilChainingOps
+import Event.*
+import cats.implicits.catsSyntaxFlatMapOps
 
-def draw(queue: Queue[Draw])(value: Draw) =
-  models.canvas.InMemory
-    .writePixel(
-      value.x,
-      value.y,
-      value.pixel,
-      value.canvasId
-    )
-  // .tap(_ => queueDraw(value)(queue))
+import util.chaining.scalaUtilChainingOps
+import models.CanvasRepo
+import cats.effect.IO
+
+object Draw {
+
+  def draw(value: Draw) =
+    models.redis.client.use { client =>
+      given models.redis.RedisEnv = client
+      models.canvas.InMemory
+        .writePixel(
+          value.x,
+          value.y,
+          value.pixel,
+          value.canvasId
+        )
+        .map(_ => models.redis.Canvas.draw(value))
+        .getOrElse(IO.pure(()))
+    }
+    // .tap(_ => queueDraw(value)(queue))
+}
